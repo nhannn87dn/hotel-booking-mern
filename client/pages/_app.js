1fc8eed6106c16f983c7d42438943b4f52e07ae5
nhannn87dn <nhannn87dn@gmail.com>
2022-09-12 18:21:40 +0700

    init servre

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,13 +1,9 @@
 
 import '../styles/globals.css';
 import '../styles/gridflex.css';
-import { useRouter } from 'next/router';
-
-
 function MyApp({ Component, pageProps }) {
 
   return (
-  
       <Component {...pageProps} />
    
   )
