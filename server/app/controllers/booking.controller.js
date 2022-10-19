7a2c149d06278dcdaf61a7bf31c7c5ca0508c6ad
nhannn87dn <nhannn87dn@gmail.com>
2022-10-19 22:13:53 +0700

    fix send mail booking

--- server/app/controllers/booking.controller.js
+++ server/app/controllers/booking.controller.js
@@ -2,7 +2,7 @@ const {bookingService} = require("../services");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require('../utils/AppError');
 const requestHandler = require("../utils/requestHandler");
-
+const nodeMailer = require('../utils/mailer');
 /**
  * Public Controllers
  */
@@ -37,6 +37,29 @@ const getBookings = catchAsync(async (req, res)=> {
 
 const createBooking = catchAsync(async (req, res)=> {
     const booking = await bookingService.createBooking(req.body);
+
+    if(booking){
+        //send Booking to mail
+        try {
+            // Lấy data truyền lên từ form phía client
+            let subject = 'Confirm Booking From Sochi Hotel';
+            let bodyHtml = `<p><b>Hello !</b></p>
+            <p>Code Booking: <strong>${booking.code}</strong></p>
+            <p>CheckIn Date: <strong>${booking.checkInDate}</strong></p>
+            <p>CheckIn Date: <strong>${booking.checkOutDate}</strong></p>
+            <p>Total Amout: <strong>${booking.totalAmount}</strong></p>
+            <p>Please contact us for supports details !</p>`;
+            // Thực hiện gửi email
+            await nodeMailer.sendMail(email, subject, bodyHtml);
+            // Quá trình gửi email thành công thì gửi về thông báo success cho người dùng
+          } catch (error) {
+            // Nếu có lỗi thì log ra để kiểm tra và cũng gửi về client
+            console.log(error)
+            //throw new AppError('Send code to email failed',400);
+          }
+    }
+
+
     requestHandler.sendSuccess(res,'successful')(booking);
 });
 
